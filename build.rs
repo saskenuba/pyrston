a2d844b7357dd9eee4a41aa491ebf43a44aee786
Martin <martin@hotmail.com.br>
2019-09-21 17:06:29 -0300

    initial commit
    Signed-off-by: Martin <martin@hotmail.com.br>

---
+++ build.rs
@@ -0,0 +1,22 @@
+use std::path::PathBuf;
+
+fn main() {
+    let dir: PathBuf = ["tree-sitter-python", "src"].iter().collect();
+
+    let mut c_builder = cc::Build::new();
+    let mut cpp_builder = cc::Build::new();
+
+    c_builder
+        .include(&dir)
+        .pic(true)
+        .warnings(false)
+        .file(dir.join("parser.c"))
+        .compile("basinga");
+
+    cpp_builder
+        .include(&dir)
+        .cpp(true)
+        .file(dir.join("scanner.cc"))
+        .compile("tree-sitter-python");
+}
+
